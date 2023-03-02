cb87a435b8f529d3cb2c021ce02d8c02d2fc9ccc
Tatsuto Gion <tatsuto.gion@gmail.com>
2023-03-02 16:32:54 +0900

    SwitchbotDefを修正

--- src/SwitchbotDef.ts
+++ src/SwitchbotDef.ts
@@ -1,19 +1,21 @@
 import { NodeDef } from 'node-red';
 
 export interface ConfigDef extends NodeDef {
+  name: string;
   token: string;
   secret: string;
+  sign: string;
 }
 
 export interface StatusDef extends NodeDef {
-  authorization: string,
+  authorization: string;
   deviceId: string;
 }
 
 export interface CommandDef extends NodeDef {
-  authorization: string,
+  authorization: string;
   deviceId: string;
-  deviceType: boolean,
-  command: string,
-  parameter: string
+  deviceType: boolean;
+  command: string;
+  parameter: string;
 }
